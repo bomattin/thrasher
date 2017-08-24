54f33d2f4ce5ffe6d5db4173b669225c72e3183c
Mattingly, Bret <Bret.Mattingly@nextgearcapital.com>
2017-08-23 23:29:13 -0400

    basic functionality

---
+++ src/filehose.rs
@@ -0,0 +1,23 @@
+use std::path::PathBuf;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use rand::os::OsRng;
+use rand::Rng;
+
+
+pub fn spam(path: PathBuf, n: i32, ext: &str) {
+    let mut rng = OsRng::new().unwrap();
+    let mut buf : Vec<u8> = vec![0x00; 4096];
+
+
+    for i in 0..n {
+        rng.fill_bytes(&mut buf);
+        let mut file = OpenOptions::new()
+                        .read(true)
+                        .write(true)
+                        .create(true)
+                        .open(format!("{}.txt", i))
+                        .unwrap();
+        file.write_all(&buf);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate clap;
+extern crate rand;
+
+mod filehose;
+
+use filehose::spam;
+use clap::{Arg, App};
+use std::path::PathBuf;
+
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("thRaSher")
+                        .version("0.1")
+                        .author("Bret Mattingly <bret.mattingly@gmail.com>")
+                        .about("Random high-speed file creation for application testing")
+                        .arg(Arg::with_name("DIRECTORY")
+                            .help("The root directory in which to create files")
+                            .required(true)
+                            .index(1))
+                        .arg(Arg::with_name("COUNT")
+                            .help("Number of files to create")
+                            .required(true)
+                            .index(2))
+                        .arg(Arg::with_name("EXTENSION")
+                            .help("File extension to use")
+                            .required(true)
+                            .index(3))
+                        .get_matches();
+
+    let directory = matches.value_of("DIRECTORY").unwrap_or("./");
+    let count_str = matches.value_of("COUNT").unwrap_or("1000");
+    let count: i32 = count_str.parse().unwrap();
+    let ext = matches.value_of("EXTENSION").unwrap_or(".txt");
+
+    spam(PathBuf::from(directory), count, ext);
+
 }
